e242f8c37a441531dda1cdf5d2cfbe68f0f0bebf
dangWangs <1028680480@qq.com>
2017-03-30 14:12:27 +0800

    x

--- systemjs.config.js
+++ systemjs.config.js
@@ -24,6 +24,7 @@
             // other libraries
             'rxjs':                       'npm:rxjs',
             'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
+            'ng2-pagination': 'npm:ng2-pagination',
         },
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
